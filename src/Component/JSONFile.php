9e06effebd997e4d1dd29772454a9c345c9c2312
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2019-01-11 02:25:25 +0330

    Add JSON::save()
    To save data to the file.

--- src/Component/JSONFile.php
+++ src/Component/JSONFile.php
@@ -10,14 +10,19 @@ class JSONFile extends JSON
     {
         $this->filePath = $filePath;
 
-        parent::__construct($this->readFile());
+        parent::__construct($this->read());
     }
 
-    protected function readFile()
+    protected function read()
     {
         if (!is_readable($this->filePath))
             throw new \Exception("File is not readable");
         
         return file_get_contents($this->filePath);
+    }
+
+    public function save(int $options = JSON_PRETTY_PRINT)
+    {
+        file_put_contents($this->data);
     }
 }
