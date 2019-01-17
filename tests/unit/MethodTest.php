8928b750fd13f6cd2d47cf40db3aff830fdd605c
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2019-01-18 02:06:54 +0330

    Improve PHPUnit tests
    Add PHPDoc comments for PHPUnit tests and improve a test a bit.

--- tests/unit/MethodTest.php
+++ tests/unit/MethodTest.php
@@ -1,4 +1,13 @@
 <?php
+/** 
+ * Unit tests for MAChitgarha\Component\JSON class.
+ *
+ * Go to the project's root and run the tests in this way:
+ * phpunit --bootstrap vendor/autoload.php tests/unit
+ * Using the --repeat option is recommended.
+ * 
+ * @see MAChitgarha\Component\JSONFile
+ */
 
 namespace MAChitgarha\UnitTest\JSONFile;
 
@@ -7,14 +16,27 @@ use MAChitgarha\Component\JSONFile;
 use Webmozart\PathUtil\Path;
 use MAChitgarha\Component\JSON;
 
+/**
+ * Tests all methods individually.
+ */
 class MethodTest extends TestCase
 {
     /** @var string Prefix path for JSON files to be loaded by JSONFile class. */
     protected $filesPrefix = __DIR__ . "/../data/";
 
-    /** @var string[] A set of JSON files to work with them. */
+    /** @var string[] A set of JSON files to work with them. {@see self::setUp()} */
     protected $files = [];
 
+    /**
+     * Prepare $this->files.
+     * 
+     * Set 'files' property to contain an array consisting JSON files' data to be used in tests.
+     * The index is the file's name without its extension, e.g., if the filename is 'empty.json',
+     * then the index of its data would be 'empty'.
+     * Each array element has two indexes:
+     * path: specifies the path of the JSON file,
+     * contents: the default file contents. {@see self::tearDown()}
+     */
     protected function setUp()
     {
         $fileData = [
@@ -23,6 +45,7 @@ class MethodTest extends TestCase
                 []
             ]
         ];
+
         foreach ($fileData as $fileDatum) {
             $name = $fileDatum[0];
             $fileContents = $fileDatum[1];
@@ -34,11 +57,17 @@ class MethodTest extends TestCase
         }
     }
 
+    /**
+     * Returns a JSONFile instance using one of the files in 'files' property.
+     */
     protected function loadJsonFile(string $filename)
     {
         return new JSONFile($this->files[$filename]["path"]);
     }
 
+    /**
+     * Tests JSONFile::save() method.
+     */
     public function testSave()
     {
         $jsonFile = $this->loadJsonFile("empty");
@@ -50,19 +79,27 @@ class MethodTest extends TestCase
 
         $jsonFile->save();
 
+        // Check if the file saved correctly or not
         $this->assertEquals(3, $this->loadJsonFile("empty")->count("apps.recent"));
-
-        return $jsonFile;
     }
 
-    public function testGetFileInfo(JSONFile $jsonFile)
+    /**
+     * Tests JSONFile::getFilename() and JSONFile::getFilePath() methods.
+     */
+    public function testGetFileInfo()
     {
-        $this->loadJsonFile("empty");
+        $jsonFile = $this->loadJsonFile("empty");
 
         $this->assertContains("tests/data/empty.json", $jsonFile->getFilePath());
         $this->assertEquals("empty.json", $jsonFile->getFilename());
     }
 
+    /**
+     * Resets all changed files.
+     *
+     * Resets all JSON files that has been changed during tests to their default contents for future
+     * tests.
+     */
     protected function tearDown()
     {
         foreach ($this->files as $file)
