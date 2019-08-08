84310c19a6d2aae83b410a52b67a8c3d1fd8e8dd
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2019-08-09 00:54:16 +0430

    Add Excepocal class file for better exception handling

---
+++ src/Util/Excepocal.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @author Mohammad Amin Chitgarha <machitgarha@outlook.com>
+ * @todo This utility should be exported as a separated component.
+ */
+
+namespace MAChitgarha\Util\Excepocal;
+
+use League\Flysystem\Adapter\Local;
+
+class Excepocal extends Local
+{
+}
