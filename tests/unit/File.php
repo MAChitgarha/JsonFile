2d62aa43bd2b6af31839cbd7328cf58cb9874fdd
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2019-12-30 01:12:49 +0330

    Add some metadata for future use

--- tests/unit/File.php
+++ tests/unit/File.php
@@ -8,6 +8,8 @@ class File
     private const dataPath = __DIR__ . "/../data";
     public const testFile = self::dataPath . "/test.json";
     public const testDir = self::dataPath . "/test";
+    public const emptyJsonFile = self::dataPath . "/empty.json";
+    public const invalidJsonFile = self::dataPath . "/invalid.json";
 
     public static function create(string $filePath, int $fileMode)
     {
